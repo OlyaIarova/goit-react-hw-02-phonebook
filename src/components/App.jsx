6b9ddf3fe23128b89be974bf23f0a7c84e16780f
OlyaIarova <domaschuk.olya@gmail.com>
2023-05-28 11:06:47 +0300

    add-hw2-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { GlobalStyle } from "./Utils/GlobalStyle.js";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import { ThemeProvider } from "styled-components";
+import { Layout } from "./Layout/Layout";
+import { theme } from './Utils/Theme';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    const { contacts } = this.state;
+
+    const checkContactName = contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (checkContactName) {
+      alert(`${newContact.name} is allready in contact!`);
+      return;
+    } else {
+      this.setState(pervState => ({
+        contacts: [...pervState.contacts, newContact],
+      }));
+    }
+  };
+
+  deleteContact = contactId => {
+    this.setState(pervState => ({
+      contacts: pervState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+
+    let normalizedFilter = filter.toLowerCase();
+
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <h1>Phonebook📘</h1>
+          <ContactForm addContact={this.addContact} />
+          <h2>Contacts📝</h2>
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            onDelete={this.deleteContact}
+          />
+          <GlobalStyle />
+        </Layout>
+      </ThemeProvider>
+    );
+  }
+}
